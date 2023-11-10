b0bccf4d5b20535ab843db36c06786179c6170ed
torcuata22 <marilynjmarquez@gmail.com>
2023-11-10 16:16:30 -0500

    routse

--- config/importmap.rb
+++ config/importmap.rb
@@ -8,3 +8,4 @@ pin "popper", to: 'popper.js', preload: true
 pin "bootstrap", to: 'bootstrap.min.js', preload: true
 pin "trix"
 pin "@rails/actiontext", to: "actiontext.js"
+pin "font-awesome", to: "font-awesome/css/all.min.css", preload: true
