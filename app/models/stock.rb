1a245ce98b128e0c082c0c9ddd0aa134a49285dd
Marilyn Marquez <marilynmarquez@Marilyns-MacBook-Pro.local>
2023-12-22 12:19:34 -0500

    working

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,6 +1,7 @@
 require 'httparty'
 
 class Stock < ApplicationRecord
+
   def self.new_lookup(symbol)
     api_key = Rails.application.credentials.alpha_vantage_client[:api_key]
     response = HTTParty.get("https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=#{symbol}&apikey=#{api_key}")
@@ -11,9 +12,11 @@ class Stock < ApplicationRecord
     if response.code == 200 && response.parsed_response['Global Quote']
       price = response.parsed_response['Global Quote']['05. price']
       name = response.parsed_response['Global Quote']['01. symbol']
-      new(ticker: symbol, name: name, last_price: price)
-    else
-      nil
+      begin
+        new(ticker: symbol, name: name, last_price: price)
+        rescue ActiveRecord::RecordInvalid
+        nil
+      end
     end
   end
 end
