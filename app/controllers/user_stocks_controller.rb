533d67650e8886590962a62e5251a7fecbd0e77b
Marilyn Marquez <marilynmarquez@Marilyns-MacBook-Pro.local>
2023-12-23 14:47:18 -0500

    add and remove implemented

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -12,4 +12,12 @@ class UserStocksController < ApplicationController
     end
   end
 
+  def destroy
+    stock = Stock.find(params[:id])
+    user_stock = UserStock.where(user_id: current_user.id, stock_id: stock.id).first
+    user_stock.destroy
+    flash[:notice]="#{stock.ticker} was successfully removed from your portfolio"
+    redirect_to my_portfolio_path
+  end
+
 end
